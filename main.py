b926070797de7c58e9c61b08141dd98a36e2261b
verzhbitsky <verzhbitsky.dm@gmail.com>
2022-01-23 22:20:30 +0200

    Task 2

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-num = int(input())
-flag = True
-for i in range(2, num):
-    if num % 2 == 0:
-        flag = False
-        break
+# num = int(input())
+# flag = True
+# for i in range(2, num):
+#     if num % 2 == 0:
+#         flag = False
+#         break
+#
+# if flag == True:
+#     print("Simple number")
+# else:
+#     print("Combined number")
 
-if flag == True:
-    print("Simple number")
-else:
-    print("Combined number")
+# result = 0
+# for i in range(5):
+#     num = int(input())
+#     if num < 0:
+#         break
+#     result += num
+# print(result)
+
+# num = int(input())
+# number = num
+# flag = False
+# while num != 0:
+#     last_digit = num % 10
+#     if last_digit == 7:
+#         flag = True
+#         break
+#     num //= 10
+# if flag == True:
+#     print('Число', number, 'содержит цифру 7')
+# else:
+#     print('Число', number, 'не содержит цифру 7')
+
+# num = int(input())
+# devider = 2
+# while True:
+#     if num % devider == 0:
+#         break
+#     devider += 1
+# print(devider)
