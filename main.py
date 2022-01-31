e2780371eb1fb4ba2bb2f3391e586cfba975a4df
verzhbitsky <verzhbitsky.dm@gmail.com>
2022-01-31 20:12:32 +0200

    Task 2

--- main.py
+++ main.py
@@ -451,8 +451,570 @@
 #     print("Цифр нет")
 
 
+# s = input()
+# counter_plus = 0
+# counter_mult = 0
+# for i in range(len(s)):
+#     if s[i] == '+':
+#         counter_plus += 1
+#     if s[i] == '*':
+#         counter_mult += 1
+# print("Символ + встречается", counter_plus, "раз")
+# print("Символ * встречается", counter_mult, "раз")
+
+# s = input()
+# counter = 0
+# for i in range(len(s) - 1):
+#     if s[i] == s[i + 1]:
+#         counter += 1
+# print(counter)
 
 
+# s = input()
+# s_low = s.lower()
+# vocal_ch = "ауоыиэяюёе"
+# unvocal_ch = "бвгджзйклмнпрстфхцчшщ"
+# counter_voc = 0
+# counter_unvoc = 0
+# for i in range(len(s_low)):
+#     for j in range(len(vocal_ch)):
+#         if s_low[i] == vocal_ch[j]:
+#             counter_voc += 1
+#
+#     for k in range(len(unvocal_ch)):
+#         if s_low[i] == unvocal_ch[k]:
+#             counter_unvoc += 1
+#
+#
+# print("Количество гласных букв равно", counter_voc)
+# print("Количество согласных букв равно", counter_unvoc)
+
+
+# num = int(input())
+# x = 0
+# s = ''
+# rev_s = ''
+# for i in range(num):
+#     x = num % 2
+#     num //= 2
+#     s += str(x)
+#     if num == 0:
+#         break
+# for j in range(1, len(s) + 1):
+#     rev_s += s[len(s) - j]
+# print(rev_s)
+
+
+
+# s = input()
+# if s[:] == s[::-1]:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# общее количество символов в строке;
+# исходную строку повторенную 3 раза;
+# первый символ строки;
+# первые три символа строки;
+# последние три символа строки;
+# строку в обратном порядке;
+# строку с удаленным первым и последним символом.
+
+# s = input()
+# print(len(s))
+# print(s * 3)
+# print(s[0:1])
+# print(s[0:3])
+# print(s[-3:])
+# print(s[::-1])
+# print(s[1:len(s) - 1])
+
+
+# третий символ этой строки;
+# предпоследний символ этой строки;
+# первые пять символов этой строки;
+# всю строку, кроме последних двух символов;
+# все символы с четными индексами;
+# все символы с нечетными индексами;
+# все символы в обратном порядке;
+# все символы строки через один в обратном порядке, начиная с последнего.
+
+# s = input()
+# print(s[2:3])
+# print(s[-2:-1])
+# print(s[:5])
+# print(s[:-2])
+# print(s[::2])
+# print(s[1::2])
+# print(s[::-1])
+# print(s[-1::-2])
+
+
+# s = input()
+# if len(s) % 2 == 0:
+#     half = len(s) // 2
+#     s1 = s[:half]
+#     s2 = s[(len(s)) // 2:]
+# else:
+#     half = (len(s) // 2) + 1
+#     s1 = s[:half]
+#     s2 = s[(len(s) + 1) // 2:]
+# print(s2 + s1)
+
+
+# name = input()
+# if name.title() == name:
+#     print("YES")
+# else:
+#     print("NO")
+
+# s = input()
+# counter = 0
+# for c in s:
+#     if c.islower() == True:
+#         counter += 1
+# print(counter)
+
+
+# s = input()
+# s = s.lower()
+# print("Аденин:", s.count('а'))
+# print("Гуанин:", s.count('г'))
+# print("Цитозин:", s.count('ц'))
+# print("Тимин:", s.count('т'))
+
+
+# n = int(input())
+# counter = 0
+# for i in range(n):
+#     s = input()
+#     if '11' in s:
+#         if s.count('11') > 2:
+#             counter += 1
+# print(counter)
+
+# s = input()
+# counter = 0
+# for c in s:
+#     if c.isnumeric():
+#         counter += 1
+# print(counter)
+
+# s = input()
+# if s.find('.com', len(s) - 4, len(s)) > 0:
+#     print("YES")
+# elif s.find('.ru', len(s) - 3, len(s)) > 0:
+#     print("YES")
+# else:
+#     print("NO")
+
+# s = input()
+# counter = 0
+# max_counter = 0
+# max_char = s[0]
+# counter = s.count(s[0])
+# max_counter = counter
+# for c in s:
+#     if max_counter <= s.count(c):
+#         max_counter = s.count(c)
+#         max_char = c
+# # print(max_counter)
+# print(max_char)
+
+
+# s = input()
+# if s.count('f') == 1:
+#     print(s.find('f'))
+# elif s.count('f') > 1:
+#     print(s.find('f'), s.find('f', s.rfind('f')))
+# else:
+#     print("NO")
+
+
+# s = input()
+# first_in = s.find('h')
+# last_in = s.rfind('h')
+# print(s.replace(s[first_in:last_in + 1], ''))
+
+
+# s = 'In {0}, someone paid {1} {2} for two pizzas.'
+# year = 2010
+# price = '10k'
+# currency = 'Bitcoin'
+# print(s.format(year, price, currency))
+
+
+# year = 2010
+# amount = '10K'
+# currency = 'Bitcoin'
+#
+# print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
+
+
+# for i in range(26):
+#     print(chr(ord('A') + i))
+
+
+
+# a = int(input())
+# b = int(input())
+# s = ''
+# for i in range(a, b + 1):
+#     s += chr(i) + ' '
+# print(s)
+
+
+# s = input()
+# new_s = ''
+# for c in s:
+#     new_s += str(ord(c)) + ' '
+# print(new_s)
+
+
+# print(chr(ord('D') - 3))
+
+# num = int(input())
+# s = input()
+# new_s = ''
+# x = 0
+# for c in s:
+#     if num > ord(c) - ord('a'):
+#         x = ord(c) - ord('a')
+#         new_s = new_s + chr((ord('z') - num) + (x + 1))
+#     else:
+#         new_s += chr(ord(c) - num)
+# print(new_s)
+
+
+# s = input()
+# n = s.count('f')
+# if n > 1:
+#     print(s.find('f', s.find('f') + 1))
+# elif n == 1:
+#     print(-1)
+# else:
+#     print(-2)
+
+
+# s = input()
+# n_s = s[s.find('h') + 1:s.rfind('h')]
+# rev_s = n_s[::-1]
+# print(s.replace(n_s, rev_s))
+
+
+# a = int(input())
+# num = list(range(1, a + 1))
+# print(num)
+
+
+
+# num = int(input())
+# chars = list()
+# for i in range(97, 97 + num):
+#     chars += chr(i)
+# print(chars)
+
+
+
+# rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+# rainbow[3] = 'Зеленый'
+# rainbow[6] = 'Фиолетовый'
+#
+# print(rainbow)
+
+
+# languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+# print(languages[::-1])
+
+
+# numbers1 = [1, 2, 3] * 2
+# numbers2 = [6] * 9
+# numbers3 = [7, 8, 9, 10, 11, 12, 13]
+# print(numbers1 + numbers2 + numbers3)
+
+
+# s = list()
+# for i in range(10):
+#     s.append(i)
+# print(s)
+
+
+# Вывел длину списка;
+# Вывел последний элемент списка;
+# Вывел список в обратном порядке (вспоминаем срезы);
+# Вывел YES если список содержит числа 5 и 17, и NO в противном случае;
+# Вывел список с удаленным первым и последним элементами.
+
+# numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1,
+#            11, 10, 10, 12, 0, 0, 6, 14, 8, 2, 12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1,
+#            15, 1, 2, 14, 16, 6, 17, 5]
+# counter = 0
+# print(len(numbers))
+# print(numbers[len(numbers) - 1])
+# print(numbers[::-1])
+# if 5 and 17 in numbers:
+#     print("YES")
+# else:
+#     print("NO")
+# del numbers[0]
+# del numbers[len(numbers) - 1]
+# print(numbers)
+
+# n = int(input())
+# languages = list()
+#
+# for i in range(n):
+#     lang = input()
+#     languages.append(lang)
+# print(languages)
+
+# abc = list()
+# for i in range(26):
+#     abc.append(chr(97 + i) * (i + 1))
+# print(abc)
+
+# num = int(input())
+# n = 0
+# abc = list()
+# for i in range(num):
+#     n = int(input())
+#     abc.append(n ** 3)
+# print(abc)
+
+# num = int(input())
+# abc = list()
+# for i in range(1, num + 1):
+#     if num % i == 0:
+#         abc.append(i)
+# print(abc)
+
+
+# num = int(input())
+# sum_num = list()
+# n = int(input())
+# n_prev = 0
+# for i in range(num - 1):
+#     n_prev = n
+#     n = int(input())
+#     sum_num.append(n_prev + n)
+# print(sum_num)
+
+
+# num = int(input())
+# n = 0
+# n_list = list()
+# for i in range(num):
+#     n = int(input())
+#     n_list.append(n)
+# del n_list[1::2]
+# print(n_list)
+
+
+
+# num = int(input())
+# s_list = list()
+# ch_str = ''
+# s = ''
+# for i in range(num):
+#     s = input()
+#     s_list.append(s)
+# find_ch = int(input())
+# for c in s_list:
+#     if len(c) > find_ch - 1:
+#         ch_str += c[find_ch - 1]
+#     else:
+#         continue
+# print(ch_str)
+
+# num = int(input())
+# s_list = list()
+# for i in range(num):
+#     s = input()
+#     s_list.extend(s)
+# print(s_list)
+
+
+# s = ['q', 'w', 't', 'b']
+# for i in range(len(s)):
+#     print(s[i], end=' ')
+
+# numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+# sum_sqr = 0
+# for i in numbers:
+#     sum_sqr = sum_sqr + i**2
+# print(sum_sqr)
+
+
+# num = int(input())
+# f = 0
+# l_x = list()
+# l_y = list()
+# for i in range(num):
+#     num = int(input())
+#     l_x.append(num)
+#     f = num ** 2 + 2 * num + 1
+#     l_y.append(f)
+# print(*l_x, sep='\n')
+# print()
+# print(*l_y, sep='\n')
+
+
+# num = int(input())
+# num_list = list()
+# for i in range(num):
+#     num = int(input())
+#     num_list.append(num)
+# for j in num_list:
+#     if j == max(num_list) or j == min(num_list):
+#         continue
+#     print(j)
+
+# num = int(input())
+# num_list = list()
+# for i in range(num):
+#     num = int(input())
+#     num_list.append(num)
+# num_list.remove(max(num_list))
+# num_list.remove(min(num_list))
+# print(*num_list, sep='\n')
+
+# s_list = list()
+# num = int(input())
+# for i in range(num):
+#     s = input()
+#     if s not in s_list:
+#         s_list.append(s)
+# for j in s_list:
+#     print(j)
+
+
+# s_list = ['A caelo usque ad BEEGEEK centrum',
+#           'A capillo usque ad ungues',
+#           'A capite ad calcem beegeek',
+#           'Ab absurdo beegeeK',
+#           'Ab BEEGEEK equis ad asinos',
+#           'Ab hoedis scindere oves']
+# n_list = list()
+# word = 'Beegeek'
+# n_word = word.lower()
+# for i in s_list:
+#     n_list.append(i.lower())
+# for j in range(len(n_list)):
+#     if n_word in n_list[j]:
+#         print(s_list[j])
+
+
+# n = int(input())
+# s_list = list()
+# n_list = list()
+# for a in range(n):
+#     s_list.append(input())
+# word = input()
+# n_word = word.lower()
+# for i in s_list:
+#     n_list.append(i.lower())
+# for j in range(len(n_list)):
+#     if n_word in n_list[j]:
+#         print(s_list[j])
+
+# num1 = int(input())
+# first_list = list()
+# for i in range(num1):
+#     first_list.append(input())
+#
+# num2 = int(input())
+# second_list = list()
+# for i in range(num2):
+#     second_list.append(input())
+#
+# counter = 0
+#
+# n_first_list = list()
+# for a in first_list:
+#     n_first_list.append(a.lower())
+#
+# n_second_list = list()
+# for b in second_list:
+#     n_second_list.append(b.lower())
+#
+# for i in range(len(n_first_list)):
+#     for j in range(len(n_second_list)):
+#         if n_second_list[j] in n_first_list[i]:
+#             counter += 1
+#             if counter == len(second_list):
+#                 print(first_list[i])
+#     counter = 0
+
+
+# num = int(input())
+# num_list = list()
+# for i in range(num):
+#     num_list.append(int(input()))
+# for j in num_list:
+#     if j < 0:
+#         print(j)
+# for j in num_list:
+#     if j == 0:
+#         print(j)
+# for j in num_list:
+#     if j > 0:
+#         print(j)
+
+
+# s = input()
+# name1 = s.split()
+# name2 = list()
+# for i in range(len(name1)):
+#     name2.extend(name1[i])
+# name1 = []
+# for j in name2:
+#     if j.isupper() == True:
+#         name1.append(j)
+# print(*name1, sep='.', end='.')
+
+
+# s = input()
+# s_list = s.split("\\")
+# for c in s_list:
+#     print(c)
+
+
+# numbers = input().split()
+# for i in range(len(numbers)):
+#     print(int(numbers[i]) * '*')
+
+
+# num = input().split('.')
+# counter = 0
+# for i in range(len(num)):
+#     if 0 <= int(num[i]) <= 255:
+#         counter += 1
+# if counter == 4:
+#     print("ДА")
+# else:
+#     print("НЕТ")
+
+# num_list = list()
+# num_list.extend(input())
+# sp = input()
+# print(sp.join(num_list))
+
+# num_list = list()
+# num_list.extend(input())
+# count = 0
+# for i in range(len(num_list)):
+#     for j in range(i + 1, len(num_list)):
+#         if num_list[j] == num_list[i]:
+#             count += 1
+# print(count)
+
+
+# n_list = [2, 2, 2, 2, 2]
+# for i in range(len(n_list)):
+#     for j in range(i + 1, len(n_list)):
+#         if n_list[j] == n_list[i]:
+#             print("YES")
 
 
 
